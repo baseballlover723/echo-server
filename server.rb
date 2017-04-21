3ff3d2e3b8c32bab488543674c886135004d8da5
Philip Ross <rosspa@rose-hulman.edu>
2017-04-21 17:16:36 -0400

    created simple rack app to deploy to aws

---
+++ server.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rack'
+
+class Server
+  def startup
+    puts 'starting the server'
+    app = Proc.new do |env|
+      ['200', {'Content-Type' => 'text/html'}, ['App has started']]
+    end
+
+    Rack::Handler::WEBrick.run app
+
+    # TODO create TCP and UDP echo servers and start them
+  end
+end
+
+Server.new.startup
